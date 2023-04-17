0e68031c5134e88d8d6a46de56b1ccc2ae15228d
jaleyerdelen <jaleyerdelen@gmail.com>
2023-04-17 20:12:16 +0300

    create category CRUD

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,4 +1,59 @@
 module Api
     class CategoriesController < ApplicationController
+        def index
+            @categories = Category.order(created_at: :desc)
+            if !@categories.blank?
+                render json: @categories, status: :ok
+            else 
+                render json: @categories, status: :bad_request
+            end
+        end
+
+        def show
+            @category = set_category
+            if !@category.blank?
+                render json: @category, status: :ok
+            else
+                render json: @category, status: :bad_request
+            end
+        end
+
+        def create
+            @category = Category.create(params_category)
+            if @category.save
+                render json: @category, status: :ok
+            else
+                render json: "could't updated", status: :bad_request
+            end
+        end
+
+        def update
+            @category = set_category
+            if @category.update(params_category)
+                render json: @category, status: :ok
+            else
+                render json: "not updated", status: bad_request
+            end
+        end
+
+        def destroy
+            @category = set_category
+            if @category.destroy
+                render json: "Deleted category", status: :ok
+            else
+                render json: "Couldn't category", status: :bad_request
+            end
+        end
+
+        private # class dışından erişilemez
+        
+        def set_category
+            @category = Category.find(params[:id])
+        end
+
+        def params_category
+            params.permit(:name)
+        end
+
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_201637) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_163858) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "products", force: :cascade do |t|
