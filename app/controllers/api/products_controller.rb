c93ddb53729432a0d26384251402cbce4fafbcef
jaleyerdelen <jaleyerdelen@gmail.com>
2023-04-17 18:11:43 +0300

    create product CRUD

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,36 @@
 module Api
- class ProductsController < ApplicationController
+class ProductsController < ApplicationController
     
- end
+    def index
+        @products = Product.where(name:"laptop").order(created_at: :asc)
+        render json: @products
+    end
+
+    def show
+        @product = Product.find(params[:id])
+        render json: @product
+    end
+
+    def create
+        @product = Product.create(product_params)
+        @product.save
+        render json: @product
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        @product.update(product_params)
+        render json: @product
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        render json: "Deleted"
+    end
+
+    def product_params
+        params.permit(:name, :description, :quantity, :price) # sadece burada yazdığımız dataları alır başka datalara izin vermez
+    end
+end
 end
